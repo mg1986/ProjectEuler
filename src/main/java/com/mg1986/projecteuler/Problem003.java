2a6fa0c3e5c52126168086b5ac231fc2885d7e19
Matthew <mattgray1986@icloud.com>
2019-02-21 18:47:47 -0600

    Added Problem comments

--- src/main/java/com/mg1986/projecteuler/Problem003.java
+++ src/main/java/com/mg1986/projecteuler/Problem003.java
@@ -1,5 +1,14 @@
 package com.mg1986.projecteuler;
 
+/**
+ Largest prime factor
+ Problem 3
+
+ The prime factors of 13195 are 5, 7, 13 and 29.
+
+ What is the largest prime factor of the number 600851475143 ?
+ */
+
 public class Problem003 {
 
     private static final long limit = 600_851_475_143L;
